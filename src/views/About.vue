30c7f48f8e0a694e89eef4426422f46d1a2c76f2
ankushduacodes <ankushduacodes@gmail.com>
2021-05-23 02:53:22 +0530

    added base card layout to routes

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <base-card>
+    <div class="about">
+      <h1>This is an about page</h1>
+    </div>
+  </base-card>
 </template>
+
+<script>
+import BaseCard from '@/components/BaseUI/BaseCard.vue';
+
+export default {
+  name: 'About',
+  components: { BaseCard },
+};
+</script>
+
+<style scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,22 @@
 <template>
-  <h1>This is Home page</h1>
+  <base-card>
+  </base-card>
 </template>
 
 <script>
+import BaseCard from '@/components/BaseUI/BaseCard.vue';
+
 export default {
   name: 'Home',
+  components: {
+    BaseCard,
+  },
 };
 </script>
 
 <style scoped>
-
+.container {
+  margin-top: 4rem;
+  margin-bottom: 4rem;
+}
 </style>
