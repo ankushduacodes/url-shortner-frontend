cc1d795e0851ecc9fd20e10241d9e5a16ae8c318
ankushduacodes <ankushduacodes@gmail.com>
2021-05-23 01:38:10 +0530

    added bootstrap to the project and new navbar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <nav-bar></nav-bar>
+
 </template>
 
+<script>
+import NavBar from '@/components/NavBar.vue';
+
+export default {
+  components: { NavBar },
+};
+</script>
+
 <style>
+* {
+  box-sizing: border-box;
+}
+html {
+  height: 100%;
+}
+body {
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -14,16 +28,13 @@
   text-align: center;
   color: #2c3e50;
 }
-
 #nav {
   padding: 30px;
 }
-
 #nav a {
   font-weight: bold;
   color: #2c3e50;
 }
-
 #nav a.router-link-exact-active {
   color: #42b983;
 }
---
+++ src/components/NavBar.vue
@@ -0,0 +1,57 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-sm">
+      <router-link class="navbar-brand" to="/"><h2>ShortenIt</h2></router-link>
+      <button class="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link
+              to="/"
+              class="nav-link"
+              aria-current="page"
+            >
+              Home
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/about" class="nav-link" aria-current="page">About</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+};
+</script>
+
+<style scoped>
+.navbar-nav {
+  margin-left: auto;
+}
+
+a,
+a:hover {
+  text-decoration: none;
+  color: inherit;
+}
+
+h2 {
+  padding: 0;
+  margin: 0;
+  font-weight: 100;
+}
+</style>
