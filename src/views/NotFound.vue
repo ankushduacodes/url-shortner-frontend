4972ce96e52b3f4fa21f946b7504cfbf3daf24ea
ankushduacodes <ankushduacodes@gmail.com>
2021-05-24 16:59:53 +0530

    added not found view

---
+++ src/views/NotFound.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <img src="../assets/404.png" alt="404 Image"/>
+    <h1>Not Found</h1>
+    <h4>Go to <router-link to="/">Home</router-link></h4>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'NotFound',
+};
+</script>
+
+<style scoped>
+.container {
+  text-align: center;
+}
+
+img {
+  height: 500px;
+  width: 500px;
+}
+</style>
