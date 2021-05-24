97396b0e8eed74a92c66502a58a85079c96e44ee
ankushduacodes <ankushduacodes@gmail.com>
2021-05-24 23:53:40 +0530

    handling vue histoty

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/views/Home.vue';
-import NotFound from '@/views/NotFound.vue';
 
 const routes = [
   {
@@ -21,7 +20,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
   linkActiveClass: 'active',
 });
