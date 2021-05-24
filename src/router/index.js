7d996483415434a2f6789c6ae89e349869c914b7
ankushduacodes <ankushduacodes@gmail.com>
2021-05-24 23:51:51 +0530

    handling vue histoty

--- src/router/index.js
+++ src/router/index.js
@@ -16,7 +16,7 @@ const routes = [
   {
     path: '/:catchAll(.*)',
     name: 'NotFound',
-    component: NotFound,
+    component: () => import(/* webpackChunkName: "notfound" */'@/views/NotFound.vue'),
   },
 ];
 
